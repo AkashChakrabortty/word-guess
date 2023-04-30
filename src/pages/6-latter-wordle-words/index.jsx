54d8c212a44b1f4bc6d9d1809979c663d19a1714
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-04-30 22:44:03 +0600

    6word backspace bug fix

--- src/pages/6-latter-wordle-words/index.jsx
+++ src/pages/6-latter-wordle-words/index.jsx
@@ -64,6 +64,7 @@ export default function Page() {
     else{
       inputValue = e.key ? e.key : 'Backspace'
     }
+    console.log(inputValue)
    
     if (inputValue == 'enter' || inputValue == 'Enter') {
       if (row1.length == 6 && !enter1) {
@@ -972,7 +973,8 @@ export default function Page() {
               </button>
             </div>
             <div className="flex w-full gap-2">
-            <button className="flex h-14 w-full cursor-pointer select-none items-center justify-center rounded-[4px] font-semibold uppercase focus:outline-none text-black dark:text-white bg-gray-30 dark:bg-gray-70 flex-[1.5]"><svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="currentColor" viewBox="0 0 44.18 44.18" height="24" width="24"><path d="M10.625,5.09L0,22.09l10.625,17H44.18v-34H10.625z M42.18,37.09H11.734l-9.375-15l9.375-15H42.18V37.09z"></path><polygon points="18.887,30.797 26.18,23.504 33.473,30.797 34.887,29.383 27.594,22.09 34.887,14.797 33.473,13.383 26.18,20.676      18.887,13.383 17.473,14.797 24.766,22.09 17.473,29.383  "></polygon></svg></button>
+            <button className={fixedClass} onClick={handleValue}><svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="currentColor" viewBox="0 0 44.18 44.18" height="24" width="24"><path d="M10.625,5.09L0,22.09l10.625,17H44.18v-34H10.625z M42.18,37.09H11.734l-9.375-15l9.375-15H42.18V37.09z"></path><polygon points="18.887,30.797 26.18,23.504 33.473,30.797 34.887,29.383 27.594,22.09 34.887,14.797 33.473,13.383 26.18,20.676      18.887,13.383 17.473,14.797 24.766,22.09 17.473,29.383  "></polygon></svg></button>
+
 
               <button className={fixedClass} onClick={handleValue} value="z">
                 z
